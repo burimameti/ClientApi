07b80a6b01f5d5da34d98a8868365b825314da37
Rob Pinna <rob.pinna@cofamilies.com>
2015-02-24 19:57:43 -0700

    map

--- ClientApi/Installers/MappingInstaller.cs
+++ ClientApi/Installers/MappingInstaller.cs
@@ -1,7 +1,9 @@
 ﻿using System.Net;
 using AutoMapper;
+using Cofamilies.ClientApi.Activations;
 using Cofamilies.ClientApi.Activities;
 using Cofamilies.ClientApi.People;
+using Cofamilies.J.Core.Activations;
 using Cofamilies.J.Core.Activities;
 using Cofamilies.J.Core.People;
 
@@ -15,6 +17,7 @@ namespace Cofamilies.ClientApi.Installers
       Mapper.CreateMap<JActivities, Activities.Activities>()
         .ForMember(d => d.ActivitiesList, opt => opt.MapFrom(s => s.Activities));
       Mapper.CreateMap<JPerson, Person>();
+      Mapper.CreateMap<JActivationCreateResult, ActivationCreateResult>();
     }
   }
 }
