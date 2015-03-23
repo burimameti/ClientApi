aaf563f0d7d9c0e51cf759c3377f8fb80d28f5f4
Rob Pinna <rob.pinna@cofamilies.com>
2015-03-22 20:16:29 -0600

    lazy create change set

--- ClientApi/Activities/Activity.cs
+++ ClientApi/Activities/Activity.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Rob.Core.ChangeSet;
 
 namespace Cofamilies.ClientApi.Activities
 {
@@ -19,5 +21,40 @@ namespace Cofamilies.ClientApi.Activities
     public string TargetId { get; set; }
     public string Title { get; set; }
     public string Verb { get; set; }
+
+    // Methods
+
+    public ChangeSet ChangeSet
+    {
+      get
+      {
+        _EnsureChangeSet();
+        return _changeSet;
+      }
+    }
+    private ChangeSet _changeSet;
+
+    private void _EnsureChangeSet()
+    {
+      if (_changeSet != null)
+        return;
+
+      var x = Deserialize(Properties) as GenericProperties;
+      _changeSet = x.changes;
+    }
+
+    #region Deserialize(string s)
+    public object Deserialize(string s)
+    {
+      try
+      {
+        return JsonConvert.DeserializeObject<GenericProperties>(s);
+      }
+      catch (Exception)
+      {
+        return new GenericProperties();
+      }
+    }
+    #endregion
   }
 }
